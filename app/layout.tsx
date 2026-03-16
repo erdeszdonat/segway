ee0f92be37d916b254bba04d9d62f7fbdd16b42e
erdeszdonat <erdeszdonatcd@gmail.com>
2026-03-16 16:26:39 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Navimow Szakértő | Robotfűnyíró Kalkulátor",
-  description: "Találja meg a kertjéhez tökéletes Segway Navimow robotfűnyírót másodpercek alatt!",
+  description: Találja meg a kertjéhez tökéletes Segway Navimow robotfűnyírót másodpercek alatt!",
 };
 
 export default function RootLayout({
@@ -11,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="hu">
-      <body className="antialiased">
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
